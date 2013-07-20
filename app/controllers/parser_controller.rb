a50607d05fb23444f6e61267e5260a2e4146134d
Edin Pjanic <pjanicedin87@gmail.com>
2013-07-20 17:05:05 +0200

    rasterize invoke helper

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -1,9 +1,30 @@
+require 'timeout'
+require 'socket'
+
 class ParserController < ApplicationController
 	def parse
 		@string = params[:csv]
 	end
 
 	def rasterize
-		`./vendor/plugins/phantomjs/bin/phantomjs vendor/plugins/phantomjs/bin/rasterize.js http://localhost:3000/parse?csv=a_s_d_f public/inphographics/test.png`
+		aws_ip = "54.218.15.104"
+		begin 
+			Timeout.timeout(5) do
+				s = TCPSocket.new("http://#{aws_ip}/invoke_rasterization")
+				s.close
+
+				puts "Everything went well"
+				return true
+			end
+		rescue Errno::ECONNREFUSED
+			puts "Connection refused"
+			return true
+		rescue Timeout::Error
+			puts "Timeout error"
+			return false
+		rescue
+			puts "Unknown error has ocured"
+			return false
+		end
 	end
 end
